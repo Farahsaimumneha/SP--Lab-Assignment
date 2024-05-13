60253e48284ee9c5f333784387a12d087c603252
Farah Saimum Neha <farahsaimumneha2004@gmail.com>
2024-05-13 14:31:46 +0600

    Add files via upload

---
+++ Assignment 1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+
+float length,width,area,perimeter;
+printf("Enter the length of the Rectangle:");
+scanf("%f",&length);
+
+printf("Enter the width of the Rectangle:");
+scanf("%f",&width);
+
+
+area=length*width;
+perimeter=2*(length+width);
+
+
+printf("Area of the Rectangle:%.2f\n",area);
+printf("perimeter of the Rectangle:%.2f\n",perimeter);
+return 0;
+
+}
+
